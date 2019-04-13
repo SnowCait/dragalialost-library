3ec9d2064af89330d56175932b80c3cc8bc7736f
SnowCait <SnowCait@users.noreply.github.com>
2019-04-14 05:45:49 +0900

    void battles
    void agni

--- src/components/VoidBattles.vue
+++ src/components/VoidBattles.vue
@@ -10,16 +10,28 @@
 </template>
 
 <script>
-import voidBattles from '../assets/json/VoidBattles.json';
+import voidBattleSchedule from '../assets/json/VoidBattleSchedule.json';
+import voidBattleScheduleForEvent from '../assets/json/VoidBattleSchedule.event.json';
 
 export default {
     data () {
         const date = new Date();
         date.setHours(date.getHours() - 15);
         const day = date.getDay();
+        const dayStrings = {
+            0: 'Sun',
+            1: 'Mon',
+            2: 'Tue',
+            3: 'Wed',
+            4: 'Thu',
+            5: 'Fri',
+            6: 'Sat'
+        };
+
+        const eventEndDate = new Date(2019, 4 - 1, 26, 15);
         return {
-            voidEnemies: voidBattles
-                .filter(battle => battle[day])
+            voidEnemies: (date < eventEndDate ? voidBattleScheduleForEvent : voidBattleSchedule)
+                .filter(battle => battle[dayStrings[day]])
                 .map(battle => {
                     return require('@/assets/img/enemy/' + battle.Boss.replace(' ', '_') + '_Enemy_Icon.png');
                 })
