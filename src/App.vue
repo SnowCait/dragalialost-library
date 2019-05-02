008f39e7b555ad3fbfdd3f35c4f367ef39b261fd
SnowCait <SnowCait@users.noreply.github.com>
2019-05-03 03:07:52 +0900

    link to home

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <router-link to="/"><img alt="Vue logo" src="./assets/logo.png"></router-link>
     <router-view />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
