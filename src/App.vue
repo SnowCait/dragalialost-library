fae476851ed893933c434e3280a0b5732d6daf06
SnowCait <SnowCait@users.noreply.github.com>
2019-06-11 02:14:56 +0900

    Footer

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,27 @@
   <div id="app">
     <router-link to="/"><img alt="Vue logo" src="./assets/logo.png"></router-link>
     <router-view />
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <footer>
+      <ul>
+        <li class="github">
+          <a href="https://github.com/SnowCait/dragalialost-library" target="_blank">
+            <img src="@/assets/img/ui/logo/GitHub-Mark-120px-plus.png" alt="GitHub" />
+          </a>
+        </li>
+        <li class="twitter">
+          <a href="https://twitter.com/snow_cait" target="_blank">
+            <img src="@/assets/img/ui/logo/Twitter_Social_Icon_Circle_Color.png" alt="Twitter" />
+          </a>
+        </li>
+      </ul>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -25,6 +35,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+footer {
+  margin: 1em auto;
+}
+footer ul {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  width: 80px;
+  margin: 0 auto;
+  padding: 0;
+}
+footer ul li {
+  width: 40px;
+}
+footer ul li img {
+  width: 32px;
+  height: 32px;
 }
 
 .thumbnails {
